86559e840da5eeda5f6cab95bd3ada8d799e18d7
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2022-12-23 10:15:28 +0000

    add ascii art + update paths

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dotfiles
 Dot files I use on my Mac zsh shell.
+
+[Link to fancy ASCII text generator](https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20)
--- e2e/setup.sh
+++ e2e/setup.sh
@@ -1,3 +1,5 @@
+# A setup script used to try upgrading selenium standalone for running end to end tests on Apple silicon macs which were previously broken
+# Deprecate now but saving here if ever similar work on a single package dependecy needs to be tested on a web app
 setup() {
     echo '\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nChecking out main\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n\n'
     eval 'git checkout main'
